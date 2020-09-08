7da93d460cfc5827abf25a019f88c5a54298aa0b
Surya Prakash <prakash1431@gmail.com>
2020-09-09 03:07:02 +0530

    finalized cd/ci with travis and herko

--- scripts/start.js
+++ scripts/start.js
@@ -1,36 +1,12 @@
-const concurrently = require('concurrently');
-const port = process.env.PORT || 4200;
 var express = require('express');
 var app = express();
 
-concurrently(
-    [
-        {
-            command: `npm run ng -- serve --port ${port} --open`,
-            name: 'NG_SERVE',
-            prefixColor: 'bgBlue.bold',
-        },
-    ],
-    {
-        prefix: 'name',
-        killOthers: ['failure', 'success'],
-    }
-).then(success, failure);
-
-function success() {
-    console.log('Success');
-}
-
-function failure() {
-    console.log('Failure');
-}
-//To run instance on port 4200 on Heroku cloud server
-app.set(port);
+app.set('port', process.env.PORT || 5000);
 
 //For avoidong Heroku $PORT error
 app.get('/', function (request, response) {
     var result = 'App is running';
     response.send(result);
-}).listen(app.get(port), function () {
-    console.log('App is running, server is listening on port ', app.get(port));
+}).listen(app.get('port'), function () {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
