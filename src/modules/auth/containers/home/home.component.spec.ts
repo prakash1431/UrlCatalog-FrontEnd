23f2b069416a72aef3216d026394403c76aac31f
Surya Prakash <prakash1431@gmail.com>
2020-09-09 02:13:08 +0530

    Fixed HomeComponent Test

--- src/modules/auth/containers/home/home.component.spec.ts
+++ src/modules/auth/containers/home/home.component.spec.ts
@@ -1,25 +1,50 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HomeComponent } from './home.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+@Component({
+  template: `
+      <sb-home [someInput]="someInput" (someFunction)="someFunction($event)"></sb-home>
+  `,
+})
+class TestHostComponent {
+  // someInput = 1;
+  // someFunction(event: Event) {}
+}
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  }));
+describe('HomeComponent', () => {
+  let fixture: ComponentFixture<TestHostComponent>;
+  let hostComponent: TestHostComponent;
+  let hostComponentDE: DebugElement;
+  let hostComponentNE: Element;
+
+  let component: HomeComponent;
+  let componentDE: DebugElement;
+  let componentNE: Element;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      TestBed.configureTestingModule({
+          declarations: [TestHostComponent, HomeComponent],
+          imports: [NoopAnimationsModule],
+          providers: [],
+          schemas: [NO_ERRORS_SCHEMA],
+      }).compileComponents();
+
+      fixture = TestBed.createComponent(TestHostComponent);
+      hostComponent = fixture.componentInstance;
+      hostComponentDE = fixture.debugElement;
+      hostComponentNE = hostComponentDE.nativeElement;
+
+      componentDE = hostComponentDE.children[0];
+      component = componentDE.componentInstance;
+      componentNE = componentDE.nativeElement;
+
+      fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display the component', () => {
+      expect(hostComponentNE.querySelector('sb-home')).toEqual(jasmine.anything());
   });
 });
