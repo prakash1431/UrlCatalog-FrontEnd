6b26d32cb157ae32f8a0796b4a6ffb5abb8d4bb3
Surya Prakash <prakash1431@gmail.com>
2020-09-09 01:26:35 +0530

    Publish for CI

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function (config) {
         client: {
             clearContext: false, // leave Jasmine Spec Runner output visible in browser
         },
+
         coverageIstanbulReporter: {
             dir: require('path').join(__dirname, './coverage/UrlCatalog-FrontEnd'),
             reports: ['html', 'lcovonly', 'text-summary'],
