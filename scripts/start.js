1bf97cf0e111ffd8b474c7f0bbcef653035fea4c
Surya Prakash <prakash1431@gmail.com>
2020-09-09 03:40:21 +0530

    heroku deploy is success now adding app routes

--- scripts/start.js
+++ scripts/start.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var app = express();
+//Install express server
+const express = require('express');
+const path = require('path');
 
-app.set('port', process.env.PORT || 5000);
+const app = express();
 
-//For avoidong Heroku $PORT error
-app.get('/', function (request, response) {
-    var result = 'App is running';
-    response.send(result);
-}).listen(app.get('port'), function () {
-    console.log('App is running, server is listening on port ', app.get('port'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/UrlCatalog-FrontEnd'));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/UrlCatalog-FrontEnd/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
